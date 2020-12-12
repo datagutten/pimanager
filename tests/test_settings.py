a24632345eb5d6459100ea23b8d1dcfdaa0f3afa
datagutten <datagutten@datagutten.net>
2020-12-12 08:53:36 +0100

    Add switchinfo to test config

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -20,6 +20,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pimanager.apps.PimanagerConfig',
+    'switchinfo.apps.SwitchinfoConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

7b5ad83293e9fca58191457b9f451ea968b032f9
Anders Birkenes <datagutten@datagutten.net>
2025-03-09 12:17:40 +0100

    Add environment variable to set LANGUAGE_CODE

--- pi_device_manager/settings.py
+++ pi_device_manager/settings.py
@@ -127,7 +127,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = os.getenv('LANGUAGE_CODE', 'en-us')
 
 TIME_ZONE = os.getenv('TZ', 'UTC')
 
